885339f7e098483ab1fbd8c22f5493f33c50c60f
vuonghuuhung <vuonghuuhung2002@gmail.com>
2024-11-15 06:17:45 +0700

    TWA SDK integrated

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useCounterContract } from "./hooks/useCounterContract";
 import { useTonConnect } from "./hooks/useTonConnect";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
   const { address, value, sendIncrement, contract_balance } =
@@ -9,23 +10,36 @@ function App() {
 
   const { connected } = useTonConnect();
 
+  const showAlert = () => {
+    WebApp.showAlert("Hey there!");
+  };
+
   return (
     <div>
       <div>
         <TonConnectButton />
       </div>
+
       <div>
         <div className="Card">
+          <b>{WebApp.platform}</b>
           <b>Our contract Address</b>
           <div className="Hint">{address?.slice(0, 30) + "..."}</div>
           <b>Our contract Balance</b>
           <div className="Hint">{contract_balance}</div>
         </div>
-
         <div className="Card">
           <b>Counter Value</b>
           <div>{value ?? "Loading..."}</div>
         </div>
+
+        <a
+          onClick={() => {
+            showAlert();
+          }}
+        >
+          Show Alert
+        </a>
 
         <div className="Card">
           {connected && (
