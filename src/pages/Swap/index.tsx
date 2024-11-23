cb13766d1520d6837bc1f9be625bbc600cb62e4e
meomeocoj <minh.nt@orai.io>
2024-11-23 21:43:40 +0700

    feat: swap pages

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -16,11 +15,12 @@ import {
   RefreshCcw,
   Settings,
 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TokenSelector from "../TokenSelector";
 import { Token } from "@/interfaces";
 import useTokenStore from "@/store/tokenStore";
 import { useCounterContract } from "@/hooks/useCounterContract";
+import { getSimulateExactInAmountOut } from "@/apis/indexer";
 
 export default function Swap() {
   const tokens = useTokenStore((state) => state.tokenList);
@@ -30,8 +30,18 @@ export default function Swap() {
   const [token0, setToken0] = useState<Token>(tokens[0]);
   const [token1, setToken1] = useState<Token>(tokens[1]);
 
-  const [amount1, setAmount1] = useState("");
-  const [amount2, setAmount2] = useState("");
+  const [amountIn, setAmountIn] = useState<string>("");
+  const [amountOut, setAmountOut] = useState<string>("");
+
+  useEffect(() => {
+    const fetchSimulate = async () => {
+      // const response = await getSimulateExactInAmountOut();
+      // setAmountOut(response.simulateAmountOut);
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      setAmountOut(amountIn);
+    };
+    fetchSimulate();
+  }, [token0, token1, amountIn]);
 
   return (
     <div className="min-h-fit bg-background p-4 flex justify-center">
@@ -63,8 +73,8 @@ export default function Swap() {
               <div className="flex justify-between mb-2">
                 <Input
                   type="number"
-                  value={amount1}
-                  onChange={(e) => setAmount1(e.target.value)}
+                  value={amountIn}
+                  onChange={(e) => setAmountIn(e.target.value)}
                   className="border-0 bg-transparent text-2xl text-primary placeholder:text-muted-foreground focus-visible:ring-0 p-0 h-auto"
                   placeholder="0"
                 />
@@ -87,7 +97,7 @@ export default function Swap() {
                 />
               </div>
               <div className="flex justify-between text-sm">
-                <span className="text-gray-500">${amount1 ? "0" : "0"}</span>
+                <span className="text-gray-500">${amountIn ? "0" : "0"}</span>
                 <span className="text-gray-500">
                   <Circle className="h-3 w-3 inline mr-1 fill-gray-500" />0 DUST
                 </span>
@@ -108,11 +118,11 @@ export default function Swap() {
               <div className="flex justify-between mb-2">
                 <Input
                   type="number"
-                  value={amount1}
-                  onChange={(e) => setAmount1(e.target.value)}
+                  value={amountOut}
+                  // onChange={(e) => setAmount1(e.target.value)}
+                  disabled={true}
                   className="border-0 bg-transparent text-2xl text-primary placeholder:text-muted-foreground focus-visible:ring-0 p-0 h-auto"
                   placeholder="0"
-
                 />
                 <TokenSelector
                   children={
@@ -133,7 +143,7 @@ export default function Swap() {
                 />
               </div>
               <div className="flex justify-between text-sm">
-                <span className="text-gray-500">${amount2 ? "0" : "0"}</span>
+                <span className="text-gray-500">${amountOut ? "0" : "0"}</span>
                 <span className="text-gray-500">
                   <Circle className="h-3 w-3 inline mr-1 fill-gray-500" />0 TON
                 </span>
