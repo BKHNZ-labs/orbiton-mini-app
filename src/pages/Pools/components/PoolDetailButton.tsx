b13eddcec2ffd1282fe1ff3503e3f159359efa54
vuonghuuhung <vuonghuuhung2002@gmail.com>
2024-12-04 22:45:46 +0700

    update

--- src/pages/Pools/components/PoolDetailButton.tsx
+++ src/pages/Pools/components/PoolDetailButton.tsx
@@ -7,25 +7,25 @@ type PoolDetailProps = {
 
 export default function PoolDetailButton({ pool }: PoolDetailProps) {
     return (
-        <Link to={`/pool/${pool.id}`} className="flex items-center space-x-2">
+        <Link to={`/pool/${pool?.id}`} className="flex items-center space-x-2">
             <div className="flex -space-x-2">
                 <img
-                    src={pool.token0.image}
-                    alt={pool.token0.name}
+                    src={pool?.token0?.image}
+                    alt={pool?.token0?.name}
                     className="w-8 h-8 rounded-full border-2 border-white"
                 />
                 <img
-                    src={pool.token1.image}
-                    alt={pool.token1.name}
+                    src={pool?.token1?.image}
+                    alt={pool?.token1?.name}
                     className="w-8 h-8 rounded-full border-2 border-white"
                 />
             </div>
             <div>
                 <span className="font-medium">
-                    {pool.token0.symbol}/{pool.token1.symbol}
+                    {pool?.token0?.symbol}/{pool?.token1?.symbol}
                 </span>
                 <span className="text-sm text-gray-500 ml-2">
-                    {pool.friendlyFee}
+                    {pool?.friendlyFee}
                 </span>
             </div>
         </Link>
